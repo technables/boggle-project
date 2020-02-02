aedae6689b34c30844726440ffeb0819ddcb85ac
Chandra <chandra@braindigit.com>
2020-02-02 12:19:18 +0545

    implemented CORS in api for request from react app

--- boggle-api/config/application.rb
+++ boggle-api/config/application.rb
@@ -16,6 +16,20 @@ module BoggleApi
     config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+         origins '*'
+         resource '*', :headers => :any, :methods => [:get, :post, :options]
+       end
+    end
+
+    # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
