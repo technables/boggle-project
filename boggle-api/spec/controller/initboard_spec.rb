e6016ae5a0b34ac6b1da091f2ff80a7acb06b2b6
Chandra <chandra@braindigit.com>
2020-02-03 23:28:40 +0545

    test cases added

---
+++ boggle-api/spec/controller/initboard_spec.rb
@@ -0,0 +1,92 @@
+require "rails_helper"
+
+describe "[Initiate a new board]", :type=> :request do
+    # sampele response
+    #     {
+    #         "message":"GAME_INITIATED",
+    #         "success":true,
+    #         "data":{
+    #             "board_data":"aexhsrrieciicttn"
+    #             }
+    #     }
+    
+
+    # Test Case 1
+    # no board type provided, parameter required
+
+    it "can not submit without board type", :focus =>true do
+        get "/v1/api/initboard?type"
+        expect(response).to have_http_status(:bad_request)  
+
+        response_body = JSON.parse(response.body)
+        expect(response_body["success"]).to eq(false)  
+
+        response_data = response_body["data"]
+        expect(response_data).to eq(nil)  
+        
+    end
+
+    # Test Case 2
+    # no board type provided, parameter empty type=
+
+    it "can not submit with empty board type", :focus =>true do
+        get "/v1/api/initboard?type="
+        expect(response).to have_http_status(:bad_request)  
+
+        response_body = JSON.parse(response.body)
+        expect(response_body["success"]).to eq(false)  
+
+        response_data = response_body["data"]
+        expect(response_data).to eq(nil)  
+        
+    end
+
+    # Test Case 3
+    # valid board type provided
+
+    it "valid response with random board data should be provided", :focus =>true do
+        type = 5
+        get "/v1/api/initboard?type=" + type.to_s
+        expect(response).to have_http_status(:ok)  
+
+        response_body = JSON.parse(response.body)
+        expect(response_body["success"]).to eq(true)  
+
+        response_data = response_body["data"]
+        expect(response_data["board_data"].length).to eq(type * type)  
+        
+    end
+
+    # Test Case 4
+    # board type provided with less than 3
+
+    it "can not submit with low board type", :focus =>true do
+        type = 1
+        get "/v1/api/initboard?type=" + type.to_s
+        expect(response).to have_http_status(:bad_request)  
+
+        response_body = JSON.parse(response.body)
+        expect(response_body["success"]).to eq(false)  
+
+        response_data = response_body["data"]
+        expect(response_data).to eq(nil)  
+        
+    end
+
+    # Test Case 5
+    # board type provided valid board type
+
+    it "can not submit with invalid board type", :focus =>true do
+        type = 'asd'
+        get "/v1/api/initboard?type=" + type
+        expect(response).to have_http_status(:bad_request)  
+
+        response_body = JSON.parse(response.body)
+        expect(response_body["success"]).to eq(false)  
+
+        response_data = response_body["data"]
+        expect(response_data).to eq(nil)  
+        
+    end
+    
+end
---
+++ boggle-api/spec/controller/processword_spec.rb
@@ -0,0 +1,113 @@
+require "rails_helper"
+
+describe "[Process Word]", :type => :request do
+
+=begin
+        Sample Response
+        {
+            "message":"MSG_EVALUATION_SUCCESS",
+            "success":true,
+            "data":{
+                "is_correct":true,
+                "score":4
+            }
+        }
+=end
+
+    
+    # Test Case 1
+    # no word provided, parameter required
+
+    it "can not submit without word", :focus =>true do
+        get "/v1/api/processword?type"
+        expect(response).to have_http_status(:bad_request)  
+
+        response_body = JSON.parse(response.body)
+        expect(response_body["success"]).to eq(false)  
+
+        response_data = response_body["data"]
+        expect(response_data).to eq(nil)  
+        
+    end
+
+    # Test Case 2
+    # no word provided, parameter empty word=
+
+    it "can not submit with empty word", :focus =>true do
+        get "/v1/api/processword?word="
+        expect(response).to have_http_status(:bad_request)  
+
+        response_body = JSON.parse(response.body)
+        expect(response_body["success"]).to eq(false)  
+
+        response_data = response_body["data"]
+        expect(response_data).to eq(nil)  
+        
+    end
+
+    #Test Case 3
+    #valid request
+
+    it "process word and provide its score", :focus => true do
+        get "/v1/api/processword?word=boggle"
+        expect(response).to  have_http_status(:ok)
+
+        response_body = JSON.parse(response.body)
+        expect(response_body["success"]).to  eq(true)
+
+        response_data = response_body["data"]
+
+        expect(response_data["is_correct"]).to eq(true)
+        expect(response_data["score"]).to eq(4) 
+    end
+
+    # Test Case 4
+    # less character word
+
+    it "provided word is with lesser character", :focus => true do
+        get "/v1/api/processword?word=js"
+        expect(response).to have_http_status(:bad_request)  
+
+        response_body = JSON.parse(response.body)
+        expect(response_body["success"]).to eq(false)  
+
+        response_data = response_body["data"]
+        expect(response_data).to eq(nil)  
+    end
+
+    # Test Case 5
+    # wrong word
+
+    it "provided wrong word ", :focus => true do
+        get "/v1/api/processword?word=asdfg"
+        expect(response).to have_http_status(:ok)  
+
+        response_body = JSON.parse(response.body);
+        expect(response_body["success"]).to  eq(true)
+
+        response_data = response_body["data"]
+
+        expect(response_data["is_correct"]).to eq(false)
+        expect(response_data["score"]).to eq(0) 
+    end
+
+    # Test Case 6
+    # long word provided but max score should be 11
+
+    it "provided wrong word ", :focus => true do
+        get "/v1/api/processword?word=abbreviations"
+        expect(response).to have_http_status(:ok)  
+
+        response_body = JSON.parse(response.body);
+        expect(response_body["success"]).to  eq(true)
+
+        response_data = response_body["data"]
+
+        expect(response_data["is_correct"]).to eq(true)
+        expect(response_data["score"]).to eq(11) 
+    end
+
+
+
+    
+end
